131a5ce857d1f551c72fae19901a3a803391aaab
Anton Slavin <slavinant@gmail.com>
2021-05-24 13:15:46 +0300

    Add background removal feature using uNet

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 let model;
+let uNet;
 let video;
+let segmentedVideo;
 
 const runButton = document.getElementById('run');
 runButton.addEventListener('click', modelReady);
 const loadingText = document.getElementById('loading');
 const resultsDiv = document.getElementById('results');
+const enableUnetCheckbox = document.getElementById('enableUnet');
+let enableUnet = enableUnetCheckbox.checked;
+
+enableUnetCheckbox.addEventListener('change', () => {
+  enableUnet = enableUnetCheckbox.checked;
+  if (enableUnet) {
+    uNet.segment(video, uNetResult);
+  }
+});
 
 function setup() {
   let canvas = createCanvas(640, 480);
   canvas.parent('sketch');
+
   video = createCapture(VIDEO);
   video.hide();
+
   model = ml5.imageClassifier('MobileNet', video, modelReady);
+  uNet = ml5.uNet('face');
+}
+
+function uNetResult(error, results) {
+  if (error) {
+    console.log(error);
+  } else {
+    segmentedVideo = results.backgroundMask;
+    if (enableUnet) {
+      uNet.segment(video, uNetResult);
+    }
+  }
 }
 
 function modelReady() {
@@ -48,5 +73,9 @@ function gotResult(error, results) {
 
 function draw() {
   background(0);
-  image(video, 0, 0);
+  if (segmentedVideo && enableUnet) {
+    image(segmentedVideo, 0, 0, 640, 480);
+  } else {
+    image(video, 0, 0);
+  }
 }
